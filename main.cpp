60c80178632d17ffbff6648f22c8a9094c5ef57f
SethHirani <seth113211@gmail.com>
2025-02-24 09:56:25 -0600

    Test

--- main.cpp
+++ main.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
 using namespace std;
+
 int ROWS = 9, COLS = 9;
 
-int main() {
-    cout << "This will be my project folder for CSII Sudoku Project." << endl;
-    cout << "This is a test" << endl;
-
-    bool isValid(int row_id, int col_id, int candidate) {
-        for (int j = 0; j < COLS; j++) {
-            if (BOARD[row_id][j] == candidate) return false;
+bool isValid(int row_id, int col_id, int candidate) {
+    for (int j = 0; j < COLS; j++) {
+        if (BOARD[row_id][j] == candidate) return false;
 
         for (int i = 0; i < ROWS; i++)
             if (BOARD[i][col_id] == candidate) return false;
 
         for (int i = (row_id/3)*3
         }
-    }
+}
+
+int main() {
+
+
+
 
     return 0;
 
